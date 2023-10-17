04d172a3069e1e6bdf290c5bdb34c5bd3491361b
Carla Muñoz <carlamunyozz@gmail.com>
2023-10-17 10:56:34 +0200

    Commit

--- .local/install/CodedWorkflow.cs
+++ .local/install/CodedWorkflow.cs
@@ -1,4 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
 using UiPath.CodedWorkflows;
+using UiPath.Core;
+using UiPath.Core.Activities.Storage;
+using UiPath.Orchestrator.Client.Models;
+using UiPath.Testing;
+using UiPath.Testing.Activities.TestData;
+using UiPath.Testing.Activities.TestDataQueues.Enums;
+using UiPath.Testing.Enums;
 
 namespace UiPathAcademy1
 {
@@ -6,7 +16,11 @@ namespace UiPathAcademy1
     {
         public CodedWorkflow()
         {
-            _ = new System.Type[]{};
+            _ = new System.Type[]{typeof(UiPath.Testing.API.ITestingService), typeof(UiPath.Core.Activities.API.ISystemService)};
         }
+
+        protected UiPath.Core.Activities.API.ISystemService system { get => serviceContainer.Resolve<UiPath.Core.Activities.API.ISystemService>() ; }
+
+        protected UiPath.Testing.API.ITestingService testing { get => serviceContainer.Resolve<UiPath.Testing.API.ITestingService>() ; }
     }
 }
